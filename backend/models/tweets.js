54457503080ffbb621815d8434c07597e1c2c751
Matthieu Saudi <saudimatthieu@gmail.com>
2026-07-16 16:47:19 +0200

    date, likes

--- backend/models/tweets.js
+++ backend/models/tweets.js
@@ -4,6 +4,8 @@ const tweetSchema = mongoose.Schema({
   message: String,
   hashtags: [String],
   username: String,
+  date: Date, 
+  likes: [String],
 });
 
 const Tweet = mongoose.model("tweets", tweetSchema);
