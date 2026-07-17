0066b3b06fcf4c8e91b55876842a80a3b2169d02
Laurent <laurent.veyrat2@gmail.com>
2026-07-17 12:08:17 +0200

    Add routes like and delete

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -2,6 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 const Tweet = require("../models/tweets");
+const User = require("../models/users");
+
+router.get("/", (req, res) => {
+  Tweet.find()
+    .sort({ date: -1 })
+    .then((data) => {
+      res.json({ result: true, tweets: data });
+    });
+});
 
 router.post("/", (req, res) => {
   if (req.body.message.length > 280) {
@@ -19,6 +28,25 @@ router.post("/", (req, res) => {
       res.json({ result: true });
     });
   }
+});
+
+router.post("/like", (req, res) => {
+  Tweet.updateOne(
+    { username: req.body.username },
+    { likes: [...likes, username] },
+  ).then((data) => {
+    if (data) {
+      res.json({ result: true });
+    } else {
+      res.json({ result: false });
+    }
+  });
+});
+
+router.post("/delete", (req, res) => {
+  Tweet.deleteOne({ username: req.body.username }).then(
+    res.json({ result: true }),
+  );
 });
 
 module.exports = router;
